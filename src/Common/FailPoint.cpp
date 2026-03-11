80828515ae1a9b508855e799e3d12ef0a832ebc8
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-03-11 22:17:34 +0000

    Backport #99205 to 25.12: Fix max_execution_time not being applied for backup/restore

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -119,6 +119,8 @@ static struct InitFiu
     REGULAR(slowdown_parallel_replicas_local_plan_read) \
     ONCE(iceberg_writes_cleanup) \
     ONCE(backup_add_empty_memory_table) \
+    PAUSEABLE_ONCE(backup_pause_on_start) \
+    PAUSEABLE_ONCE(restore_pause_on_start) \
     PAUSEABLE(sc_state_application_pause) \
     REGULAR(sc_intentions_commit_fail) \
     REGULAR(sleep_in_logs_flush) \
